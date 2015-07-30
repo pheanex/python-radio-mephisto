3e0a798a1b3df2eef780ad970f4548e1539c2bac
Konstantin Manna <Konstantin@Manna.uno>
2015-07-30 17:27:04 +0200

    made code PEP8 cleaner

--- PythonRadioCode.py
+++ PythonRadioCode.py
@@ -15,10 +15,10 @@ def get_file_to_play(zeit):
     f.close()
     for x in lines:
         cfg = string.split(x)
-        if (cfg[0] == "%04d" % (zeit.year)):
-            if (cfg[1] == "%02d" % (zeit.month)):
-                if (cfg[2] == "%02d" % (zeit.day)):
-                    if (cfg[3] == "%02d" % (zeit.hour)):
+        if cfg[0] == "%04d" % zeit.year:
+            if cfg[1] == "%02d" % zeit.month:
+                if cfg[2] == "%02d" % zeit.day:
+                    if cfg[3] == "%02d" % zeit.hour:
                         if cfg[4] == "W":
                             break
                         if cfg[4] == "V":
@@ -41,7 +41,7 @@ def get_file_to_play(zeit):
         # log("Suche Datei fuer: " + str(pdt))
         while pdt >= DateTime(string.atoi(cfg[5], 10), string.atoi(cfg[6], 10), string.atoi(cfg[7], 10), string.atoi(cfg[8], 10), 0, 0):
             filename = "/opt/dserv/data/%s/%s/%s" % (cfg[5], cfg[6], cfg[7])
-            filename = filename + "/%s-%02d-%02d.mp3" % (cfg[8], pdt.minute, pdt.second)
+            filename += "/%s-%02d-%02d.mp3" % (cfg[8], pdt.minute, pdt.second)
             if os.path.exists(filename):
                 # log("Datei gefunden: " + filename)
                 return filename
@@ -50,22 +50,22 @@ def get_file_to_play(zeit):
     # log("Keine Datei für den Eintrag gefunden! Spiele Endlosband")
     return get_endlosband()
 
-def get_endlosband():
-  global last_endlosband_hour, endlosband, last_playing
-  endlosband_now = now()
-  endlosband_hour = endlosband_now.hour
-  if (endlosband_hour <> last_endlosband_hour):
-    last_endlosband_hour = endlosband_hour
-    return get_random_file(endlosband)
 
-  return last_playing
+def get_endlosband():
+    global last_endlosband_hour, endlosband, last_playing
+    endlosband_now = now()
+    endlosband_hour = endlosband_now.hour
+    if endlosband_hour != last_endlosband_hour:
+        last_endlosband_hour = endlosband_hour
+        return get_random_file(endlosband)
+
+    return last_playing
+
 
 def get_random_file(directory):
-  result = directory + "/" + random.choice(os.listdir(directory))
-  log("Setze zufälliges Endlosband: " + result)
-  return result
-
-
+    result = directory + "/" + random.choice(os.listdir(directory))
+    log("Setze zufälliges Endlosband: " + result)
+    return result
 
 
 log_file = "/var/log/doku/doku-player.log"
@@ -77,13 +77,12 @@ timed_play = "/opt/doku/bin/timed_play"
 player_control_file = "/var/doku/computer-ist-auf-sendung"
 
 
-#Meine Funktion:
-def create_playlist(file):
+# Meine Funktion:
+def create_playlist():
     client.connect(**mpd_server)
     client.clear()
     client.add(get_file_to_play(zeit))
     client.close()
 
-
-#Auführen:
+# Auführen:
 create_playlist()
